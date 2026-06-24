e82105145296bf30af5667001d63125220cf2104
Mohammed Atef <122639481+Mohammed-Atef2004@users.noreply.github.com>
2026-06-24 19:43:14 +0300

    install components and bootsrap

--- session1/src/app/app-module.ts
+++ session1/src/app/app-module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './Components/navbar/navbar';
+import { Home } from './Components/home/home';
+import { Footer } from './Components/footer/footer';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Navbar, Home, Footer],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
